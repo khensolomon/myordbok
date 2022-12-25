c89d8a2b2cc2a083a347787f98f2ea167a2ec553
Khen Solomon Lethil <khensolomon@gmail.com>
2022-12-25 08:28:45 +0100

    working directory

--- assist/admin/csv.js
+++ assist/admin/csv.js
@@ -1,113 +1,187 @@
-const app = require('..');
-const {glossary,dictionaries} = app.Config;
+import {db,load,seek} from 'lethil';
+import path from 'path';
+import {config,  glossary} from '../anchor/index.js';
+import * as csv from 'csv';
 
-const fs = require('fs');
-// const util = require('util');
-// const path = require('path');
+const {dictionaries, table} = config.setting;
 
-// const {utility} = app.Common;
+const outputDirectory = '/dev/lidea/assets/tmp/';
 
-/*
-TODO
-1. check line
-2. parse
-3. Grammar
-4. Exist
-*/
-
-const {table,docket,makeup,fileName,chat,language} = require("./dictionary.Config");
-const partOfSpeech = require("./dictionary.pos");
-const dictionary = require("./dictionary");
-
-exports.testing = async function(param){
-  const data = fs.readFileSync('./test/FSIN_English_Myanmar_Dictionary.csv', 'UTF-8');
-  const dataLines = data.split(/\r?\n/);
-  const raw=[];
-  for (let index = 0; index < dataLines.length; index++) {
-    var row = {};
-    var line = dataLines[index];
-    var val = line.match(/("[^"]*")|[^,]+/g);
-    // var val = line.split(',');
-    row.word = val[0].replace(/"/g,"").trim();
-    row.tid = '????';
-    row.sense = val[1].trim();
-    row.exam = '';
-    row.seq = 0;
-    row.kid = 31;
-
-    // var test = await hasDefinition(row.word);
-    var hasWordAlready = await sqlFindWord(row.word);
-
-    if (hasWordAlready.length){
-      console.log('--exist-',row.word);
-    } else {
-      // row.tid = await hasPos(row.word);
-      // row.id = await sqlGetNullId();
-      // if (row.id > 0){
-      //   await sqlUpdate(row);
-      //   console.log('--update-',row.id,row.word);
-      // } else {
-      //   await sqlInsert(row);
-      //   console.log('--insert-',row.word);
-      // }
-      console.log('--todo-',row.word);
-    }
-  }
-
-};
-
-// const hasDefinition = async function(keyword){
-//   var test = await dictionary.wordFind(keyword);
-//   return test;
-// }
-
-const hasPos = async function(keyword){
-  var wordPos = await partOfSpeech.admin_pos(keyword);
-  var wordBase = await partOfSpeech.admin_base(keyword);
-  if (wordPos.pos.length){
-    return wordPos.pos[0].t;
-  } else if (wordBase.pos.length) {
-    return wordBase.pos[0].t;
-  } else if (keyword == keyword.toUpperCase()) {
-    // NOTE: probably Abbreviation
-    return 8;
-  } else if (/\s/g.test(keyword)) {
-    // NOTE: probably Phrase
-    return 11;
-  } else {
-    console.log(keyword,'pos?');
-    return 0;
-  }
-  // return wordPos;
+/**
+ * Word -> wordListUnique, wordMapDerive
+ * Sense -> senseList
+ * Thesaurus -> thesaurusList
+ * @param {any} req
+ * SQLite testing
+ */
+export async function main(req){
+  await wordListUnique(req);
+  await wordMapDerive(req);
+  await senseList(req);
+  await thesaurusList(req);
+  return 'done';
 }
 
-const sqlFindWord = async function(word){
-  return await app.sql.query("SELECT word AS term, tid AS pos, sense AS v,exam FROM ?? WHERE LOWER(word) LIKE LOWER(?) ORDER BY tid, seq;",[table.senses,word]);
+/**
+ * export word CSV -> list
+ * @param {any} req
+ * returns {Promise<String>} Promise array
+ */
+export async function wordListUnique(req){
+  const fileName = '-delete-word-list-unique.csv';
+  const writeStream = seek.writeStream(path.join(outputDirectory,fileName));
+  const stringifier = csv.stringify({
+    header: true,
+    columns: ["id", "word", "derived"],
+    quoted: true,
+    escape:'"'
+  });
+  listOfWord().then((raw)=>{
+    raw.forEach((v) => stringifier.write(v));
+  }).finally(()=>{
+    console.log('word-list.unique:', fileName);
+  });
+  // [
+  //   {id:'1',word:'a/b',sense:'3',exam:'4',wseq:'5'},
+  //   {id:'2',word:'c,d',sense:'2',exam:'3',wseq:'4'},
+  //   {id:'30',word:'c\\d',sense:'2',exam:'3',wseq:'4'},
+  //   {wseq:'30',word:'c"a"',sense:'2',exam:'3',id:'410'},
+  // ].forEach((v) => stringifier.write(v));
+  // stringifier.pipe(writeStream);
+  stringifier.pipe(writeStream);
+  return 'done';
 }
 
-const sqlInsert = async function(val){
-  await app.sql.query('INSERT INTO ?? (word, tid, sense, exam, seq, kid) VALUES (?,?,?,?,?,?);',[
-    'senses_test',val.word,val.tid,val.sense,val.exam,val.seq, val.kid
-  ]).catch(
-    e=>console.error(e.message)
+/**
+ * export derive CSV -> map
+ * @param {any} req
+ */
+export async function wordMapDerive(req){
+  const fileName = '-delete-word-map-derive.csv';
+  const writeStream = seek.writeStream(path.join(outputDirectory,fileName));
+  const stringifier = csv.stringify({
+    header: true,
+    columns: ["id", "dete", "wrid", "wrig", "wrig"],
+    quoted: true,
+    escape:'"'
+  });
+  listOfDerive().then((raw)=>{
+    raw.forEach((v) => stringifier.write(v));
+  }).finally(()=>{
+    console.log('word-map.derive:', fileName);
+  });
+  stringifier.pipe(writeStream);
+  return 'done';
+}
+
+/**
+ * export sense CSV -> list
+ * @param {any} req
+ */
+export async function senseList(req){
+  const fileName = '-delete-sense-list-all.csv';
+  const writeStream = seek.writeStream(path.join(outputDirectory,fileName));
+  const stringifier = csv.stringify({
+    header: true,
+    columns: ["id", "word", "sense", "exam", "wseq"],
+    quoted: true,
+    escape:'"'
+  });
+  listOfSense().then((raw)=>{
+    raw.forEach((v) => stringifier.write(v));
+  }).finally(()=>{
+    console.log('sense-list.all:', fileName);
+  });
+  stringifier.pipe(writeStream);
+  return 'done';
+}
+
+/**
+ * export thesaurus CSV -> list
+ * @param {any} req
+ */
+export async function thesaurusList(req){
+  const fileName = '-delete-thesaurus-list-all.csv';
+  const writeStream = seek.writeStream(path.join(outputDirectory,fileName));
+  const stringifier = csv.stringify({
+    header: true,
+    columns: ["wrid", "wlid"],
+    quoted: true,
+    escape:'"'
+  });
+  listOfThesaurus().then((raw)=>{
+    raw.forEach((v) => stringifier.write(v));
+  }).finally(()=>{
+    console.log('thesaurus-list.all:', fileName);
+  });
+  stringifier.pipe(writeStream);
+  return 'done';
+}
+
+/**
+ * Word list -> list
+ * @typedef {Object} listOfWord
+ * @property {number} id
+ * @property {string} word
+ * @property {number} derived
+ *
+ * @returns {Promise<Array.<listOfWord>>} Promise array
+ */
+async function listOfWord(){
+  return db.mysql.query(
+    "SELECT id, word, derived FROM ??;",
+    [table.synset]
   );
 }
 
-const sqlUpdate = async function(val){
-  // table.senses,row.word
-  await app.sql.query('UPDATE ?? SET word=?, tid=?, sense=?, exam=?, seq=?, kid=? WHERE `id`=?;',[
-    'senses_test',val.word,val.tid,val.sense,val.exam,val.seq, val.kid,val.id
-  ]).catch(
-    e=>console.error(e.message)
+/**
+ * Derive list -> map
+ * @typedef {Object} listOfDerive
+ * @property {number} id
+ * @property {number} dete
+ * property {string} word
+ * @property {number} wrid
+ * @property {number} wrig
+ * @property {number} wrte
+ *
+ * @returns {Promise<Array.<listOfDerive>>} Promise array
+ */
+async function listOfDerive(){
+  return db.mysql.query(
+    "SELECT id, dete, wrid, wrig, wrte FROM ?? ORDER BY wrid, wrte ASC;",
+    [table.synmap]
   );
 }
-const sqlGetNullId = async function(){
-  // table.senses,row.word
-  var raw = await app.sql.query('SELECT id FROM ?? WHERE sense IS NULL LIMIT 1;',['senses_test']).catch(
-    e=>console.error(e.message)
+
+/**
+ * Sense list -> list
+ * @typedef {Object} listOfSense
+ * @property {number} id
+ * @property {string} word
+ * @property {string} sense
+ * @property {string} exam
+ * @property {number} wseq
+ *
+ * @returns {Promise<Array.<listOfSense>>} Promise array
+ */
+async function listOfSense(){
+  return db.mysql.query(
+    "SELECT id, word, sense, exam, wseq FROM ?? WHERE word IS NOT NULL AND sense IS NOT NULL ORDER BY word, wseq ASC;",
+    [table.senses]
   );
-  if (raw.length) {
-    return raw[0].id;
-  }
-  return 0;
+}
+
+/**
+ * thesaurus list -> list
+ * @typedef {Object} listOfThesaurus
+ * @property {number} wrid
+ * @property {number} wlid
+ *
+ * @returns {Promise<Array.<listOfThesaurus>>} Promise array
+ */
+async function listOfThesaurus(){
+  return db.mysql.query(
+    "SELECT wrid, wlid FROM ??;",
+    [table.thesaurus]
+  );
 }
