3b9b435172ee2ced3fa7c0b6b192383785b838b7
Khen Solomon Lethil <khensolomon@gmail.com>
2022-12-27 15:23:09 +0100

    @pwd ts
    lighthouse

--- assets/script/sw.register.js
+++ assets/script/sw.register.js
@@ -1,7 +1,10 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js').then(function (registration) {
-    console.log("sw",registration);
-  }).catch(function (err) {
-    console.log("sw", err);
-  });
+if ("serviceWorker" in navigator) {
+	navigator.serviceWorker
+		.register("/sw.js")
+		.then(function(registration) {
+			console.log("sw", registration);
+		})
+		.catch(function(err) {
+			console.log("sw", err);
+		});
 }
