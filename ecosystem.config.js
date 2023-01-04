65ada5fd1a4e52f8aa24ee9166ff3f51ed676452
Khen Solomon Lethil <khensolomon@gmail.com>
2023-01-04 16:48:16 +0100

    export default

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,46 @@
-module.exports = {
+// module.exports = {
+// 	apps: [
+// 		{
+// 			name: "MyOrdbok",
+// 			script: "./serve.js",
+// 			watch: false,
+// 			ignore_watch: ["[\\/\\\\]\\./", "node_modules"],
+// 			env: {
+// 				NODE_ENV: "production"
+// 			},
+// 			env_production: {
+// 				NODE_ENV: "production"
+// 			},
+// 			error_file: "./log/pm2.err.log",
+// 			out_file: "./log/pm2.out.log",
+
+// 			instances: 1,
+// 			exec_mode: "cluster"
+// 		}
+// 		// {
+// 		//   name: "live",
+// 		//   script: "./serve.js",
+// 		//   instances: 1,
+// 		//   exec_mode: "cluster"
+// 		// }
+// 	]
+// };
+
+export default {
 	apps: [
 		{
 			name: "MyOrdbok",
 			script: "./serve.js",
-			// watch: true,
-			// ignore_watch:["[\\/\\\\]\\./", "node_modules"],
+			watch: false,
+			ignore_watch: ["[\\/\\\\]\\./", "node_modules"],
 			env: {
 				NODE_ENV: "production"
 			},
 			env_production: {
 				NODE_ENV: "production"
 			},
-			error_file: "./test/pm2.err.log",
-			out_file: "./test/pm2.out.log",
+			error_file: "./log/pm2.err.log",
+			out_file: "./log/pm2.out.log",
 
 			instances: 1,
 			exec_mode: "cluster"
