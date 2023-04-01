cea05646adc8b98a36ae3c90cebaece0cd059a5e
Khen Solomon Lethil <khensolomon@gmail.com>
2023-04-01 13:33:13 +0200

    add slashes

--- routes/api.js
+++ routes/api.js
@@ -22,7 +22,7 @@ routes.register("", (_req, res) => {
 		development: config.development
 	});
 });
-routes.register("nav", (_req, res) => {
+routes.register("/nav", (_req, res) => {
 	// res.send({
 	// 	name: config.name,
 	// 	version: config.version,
@@ -31,15 +31,15 @@ routes.register("nav", (_req, res) => {
 	res.json(res.locals.nav);
 });
 
-routes.register("config", (_req, res) => {
+routes.register("/config", (_req, res) => {
 	res.json(config);
 });
 
-routes.register("search", (req, res) => {
+routes.register("/search", (req, res) => {
 	search(req).then(raw => res.json(raw));
 });
 
-routes.register("speech", (req, res) => {
+routes.register("/speech", (req, res) => {
 	res.set({
 		"Content-Type": "audio/mpeg",
 		"Accept-Ranges": "bytes",
@@ -56,26 +56,26 @@ routes.register("speech", (req, res) => {
 });
 
 // req.cookies.solId
-routes.register("suggestion", (req, res) => {
+routes.register("/suggestion", (req, res) => {
 	suggestion(req.query.q, res.locals.sol.id)
 		.then(raw => res.json(raw))
 		.catch(e => res.status(404).end(e.message));
 });
 
 // 150-180ms upto 555ms
-routes.register("grammar", (req, res) => {
+routes.register("/grammar", (req, res) => {
 	grammar
 		.main(req.query.q)
 		.then(raw => res.json(raw))
 		.catch(() => res.json([]));
 });
-routes.register("grammar/pos", (req, res) => {
+routes.register("/grammar/pos", (req, res) => {
 	grammar
 		.pos(req.query.q)
 		.then(raw => res.json(raw))
 		.catch(() => res.json([]));
 });
-routes.register("grammar/base", (req, res) => {
+routes.register("/grammar/base", (req, res) => {
 	grammar
 		.base(req.query.q)
 		.then(raw => res.json(raw))
@@ -83,7 +83,7 @@ routes.register("grammar/base", (req, res) => {
 });
 
 // // /orths-:name
-// routes.register('orth', (req, res) => {
+// routes.register('/orth', (req, res) => {
 //   // req.params.name req.query.name
 //   assist.orthCharacter(req.query.name).then(
 //     raw=> res.send(raw)
@@ -92,28 +92,28 @@ routes.register("grammar/base", (req, res) => {
 //   )
 // });
 // // orthword orthord orthnse orthble ortheak
-// routes.register('orth-word', (req, res) => {
+// routes.register('/orth-word', (req, res) => {
 //   assist.orthWord(req.query.ord).then(
 //     raw=> res.send(raw)
 //   ).catch(
 //     ()=>res.status(404).end()
 //   )
 // });
-// routes.register('orth-sense', (req, res) => {
+// routes.register('/orth-sense', (req, res) => {
 //   assist.orthSense(req.query.ord).then(
 //     raw=> res.send(raw)
 //   ).catch(
 //     ()=>res.status(404).end()
 //   )
 // });
-// routes.register('orth-syllable', (req, res) => {
+// routes.register('/orth-syllable', (req, res) => {
 //   assist.orthSyllable(req.query.str).then(
 //     raw=> res.send(raw)
 //   ).catch(
 //     ()=>res.status(404).end()
 //   )
 // });
-// routes.register('orth-break', (req, res) => {
+// routes.register('/orth-break', (req, res) => {
 //   assist.orthBreak(req.query.str).then(
 //     raw=> res.send(raw)
 //   ).catch(
