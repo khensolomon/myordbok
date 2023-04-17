8bced134cc63f2a4e56f35037a149d4fcfb1ad12
Khen Solomon Lethil <khensolomon@gmail.com>
2023-04-17 18:21:47 +0200

    Update run

--- run.js
+++ run.js
@@ -1,23 +1,22 @@
-// NOTE: Please mind the reserved keywords (sql,mongo,Config,args,etc) in module.exports
-// node command myordbok
-// node run myordbok test
-// node run  myordbok
-// npm run myordbok
-// process.argv.splice(2),__dirname
 import core from "./core.js";
 import "./assist/command.js";
 
+/**
+ * Please mind the reserved keywords (sql,mongo,Config,args,etc) in module.exports
+ * @example
+ * node run [?]
+ * process.argv.splice(2),__dirname
+ */
 const app = core.command();
 app.environment();
 
+/**
+ * const usage = app.memoryUsage();
+ * for (var name in usage) console.log(usage, app.byteToMB(usage[name]), "mb");
+ */
 app.listen(function(msg) {
-	// const usage = app.memoryUsage();
-	// for (var name in usage) console.log(usage, app.byteToMB(usage[name]), "mb");
 	if (msg) {
 		console.log("...", msg);
-		// app.exit();
-	} else {
-		// app.exit(1);
 	}
 });
 
