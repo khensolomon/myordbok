823d8747a9b04d3ea76dac92944c08634476b336
Khen Solomon Lethil <khensolomon@gmail.com>
2023-04-17 18:20:37 +0200

    clear command

--- assist/command.js
+++ assist/command.js
@@ -1,7 +1,6 @@
 import { command } from "lethil";
 import { thuddar } from "./anchor/index.js";
-import * as dictionary from "./admin/dictionary.js";
-import * as gist from "./admin/gist.js";
+// import * as gist from "./admin/gist.js";
 import * as working from "./admin/working.js";
 import * as sqlite from "./admin/sqlite.js";
 
@@ -9,8 +8,8 @@ const app = command();
 const routes = app.routes();
 
 // routes.register;
-routes.register("/", () => "?");
-routes.register("/apple", () => "Did you know apple is fruit?");
+routes.register("", () => "?");
+routes.register("apple", () => "Did you know apple is fruit?");
 routes.register("orange", () => "Orange is good for health");
 routes.register("req", req => req);
 
@@ -24,47 +23,30 @@ routes.register("environment", async function(req) {
 
 routes.register("export-grammar", thuddar.update);
 
-// routes.register("testing", dictionary.testing);
-routes.register("/testings", async function(req) {
-	return await dictionary.testing(req);
-});
-// routes.register("/testings", req => "this is testing");
-routes.register("testing/:word?", dictionary.testing);
-routes.register("export-definition", dictionary.exportDefinition);
-routes.register("export-translation", dictionary.exportTranslation);
-routes.register("export-synset", dictionary.exportWordSynset);
-routes.register("export-synmap", dictionary.exportWordSynmap);
-
 routes.register("export-sqlite-test", sqlite.main);
 
-routes.register("gist-get", gist.get);
-routes.register("gist-list", gist.list);
-routes.register("gist-patch", gist.patch);
-routes.register("gist-remove", gist.remove);
-
 routes.register("works", working.main);
-
-// routes.register("wordbreak", working.main);
-// routes.register("wordbreak", function(req) {
-// 	console.log(req);
-// 	return req.route.searchParams.get("info");
-// });
 
 routes.register("wordbreak", async function(req) {
 	return import("./wordbreak/test.js").then(e => e.default(req));
 });
 
-routes.register("flat/:task?", async function(req) {
-	return import("./admin/flat.js").then(async e => await e.default(req));
+routes.register("cast/:task?/:name?", async function(req) {
+	return import("./cast/index.js").then(e => e.default(req));
+});
+
+routes.register("gist/:task?/:name?", async function(req) {
+	return import("./gist/index.js").then(e => e.default(req));
+});
+
+routes.register("flat/:task?/:name?", async function(req) {
+	return import("./flat/index.js").then(e => e.default(req));
 });
 
 routes.register("saing/:task?", async function(req) {
 	return import("./saing/index.js").then(e => e.default(req));
 });
-routes.register("sea/:task-:name?", async function(req) {
-	return import("./sea/index.js").then(e => e.default(req).catch(console.log));
-});
 
-// routes.register("upgrade/:id?", req =>
-// 	import("./admin/upgrade.md").then(e => e.default(req))
-// );
+routes.register("sea/:task?/:name?", async function(req) {
+	return import("./sea/index.js").then(e => e.default(req));
+});
