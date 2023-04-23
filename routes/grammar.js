904c966677657d616df65a7929360dc970f05dc1
Khen Solomon Lethil <khensolomon@gmail.com>
2023-04-23 10:50:56 +0200

    change import

--- routes/grammar.js
+++ routes/grammar.js
@@ -1,12 +1,13 @@
 import { server } from "lethil";
-import { thuddar } from "../assist/index.js";
+// import { thuddar } from "../assist/index.js";
+import { snap, partOfSpeech } from "../assist/thuddar/index.js";
 
 const app = server();
 const routes = app.routes("/grammar", "page");
 
 routes.register({ name: "grammar", text: "Grammar" }, async function(req, res) {
 	// var grammar = await assist.grammarMain();
-	var result = await thuddar.main();
+	var result = await snap();
 	res.render("grammar", {
 		title: result.context.name,
 		description: result.context.desc,
@@ -16,8 +17,7 @@ routes.register({ name: "grammar", text: "Grammar" }, async function(req, res) {
 });
 
 routes.register("/:id", function(req, res) {
-	thuddar
-		.pos(req.params.id)
+	partOfSpeech(req.params.id)
 		.then(function(result) {
 			if (Object.keys(result).length) {
 				var keywords = result.kind.map(e => e.root.name);
@@ -30,98 +30,10 @@ routes.register("/:id", function(req, res) {
 					grammar: result
 				});
 			} else {
-				res.status(404);
+				res.status(404).end();
 			}
 		})
 		.catch(function() {
-			res.status(404);
+			res.status(404).end();
 		});
 });
-
-/*
-const app = require('..');
-const assist = require('../assist');
-const routes = app.Router();
-
-routes.register('/pos-:id', async function(req, res) {
-  res.redirect(301, '/grammar/' + req.params.id.toLowerCase())
-});
-routes.register('/:id', async function(req, res) {
-  var id = req.params.id;
-  assist.grammarPos(id).then(function(grammar){
-    if (Object.keys(grammar).length) {
-      var keywords = grammar.kind.map(e=>e.root.name);
-      keywords.unshift(grammar.root.name,grammar.info.name);
-
-      // for (const parent of grammar.kind) {
-      //   if (parent.hasOwnProperty('word')) parent.word=[];
-      //   if (parent.hasOwnProperty('exam')) parent.exam=[];
-      //   if (parent.hasOwnProperty('kind')) {
-      //     for (const child of parent.kind) {
-      //       if (child.hasOwnProperty('word')) child.word=[];
-      //       if (child.hasOwnProperty('exam')) child.exam=[];
-      //     }
-      //   }
-      // }
-      // grammar.word=[];
-      // grammar.exam=[];
-
-      res.render('grammar-pos', {
-        title: grammar.root.name,
-        keywords: keywords.join(','),
-        description: grammar.root.desc.replace(/'/g,""),
-        grammar: grammar
-      });
-      // res.send(res.locals)
-    } else {
-      res.send('404-');
-      // res.redirect('/grammar');
-    }
-  }).catch(function(){
-    res.send('404+');
-    // res.redirect('/grammar');
-  });
-
-});
-
-routes.register('/pos-:id', async function(req, res) {
-  var id = req.params.id;
-  var grammar = await assist.grammarPos(id);
-  if (Object.keys(grammar).length) {
-    // var pos = grammar.pos[id];
-    res.render('grammar-pos', {
-      title: grammar.root.name,
-      keywords: '0, 1'.replace(0,id).replace(1,grammar.root.name),
-      description: grammar.root.desc,
-      grammar: grammar
-    });
-  } else {
-    res.send({'pos-not-found':id});
-  }
-  // var grammar = await assist.grammarPos(id);
-  // res.send({'pos-not-found':grammar});
-
-  // var grammar = await assist.grammar();
-  // if (grammar.pos.hasOwnProperty(id)) {
-  //   var pos = grammar.pos[id];
-  //   res.render('grammar-pos', {
-  //     title: pos.root.name,
-  //     keywords: '0, 1'.replace(0,id).replace(1,pos.root.name),
-  //     description: pos.root.desc,
-  //     grammar: pos
-  //   });
-  // } else {
-  //   res.send({'pos-not-found':id});
-  // }
-});
-
-routes.register('/', async function(req, res) {
-  var grammar = await assist.grammarMain();
-  res.render('grammar', {
-    title: grammar.context.name,
-    description: grammar.context.desc,
-    keywords: grammar.pos.map(e=>e.root.name).join(','),
-    grammar: grammar
-  });
-});
-*/
