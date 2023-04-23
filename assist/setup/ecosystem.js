a9e1dec1674b97023f25d8e8bbc3a3fbda7a9a30
Khen Solomon Lethil <khensolomon@gmail.com>
2023-04-23 10:37:57 +0200

    setup
    development config for ecosystem & environment

---
+++ assist/setup/ecosystem.js
@@ -0,0 +1,8 @@
+import { deploy } from "lethil";
+
+/**
+ * Purely for deployment from local/workspace
+ * @alias deploy.ecosystem
+ */
+// export default const createOrUpdate = deploy.ecosystem.createOrUpdate;
+export default deploy.ecosystem.createOrUpdate;
