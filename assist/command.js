54de8a0d35a4f5245f3d15a2f58a8de62eaf3d93
Khen Solomon Lethil <khensolomon@gmail.com>
2023-04-23 10:54:12 +0200

    improved structure

--- assist/command.js
+++ assist/command.js
@@ -1,52 +1,53 @@
 import { command } from "lethil";
-import { thuddar } from "./anchor/index.js";
-// import * as gist from "./admin/gist.js";
-import * as working from "./admin/working.js";
-import * as sqlite from "./admin/sqlite.js";
 
 const app = command();
 const routes = app.routes();
 
-// routes.register;
 routes.register("", () => "?");
 routes.register("apple", () => "Did you know apple is fruit?");
 routes.register("orange", () => "Orange is good for health");
 routes.register("req", req => req);
 
-routes.register("ecosystem", async function(req) {
-	return import("./admin/deployment.js").then(e => e.createOrUpdate(req));
+routes.register("setup/:task?", async function(req) {
+	return import("./setup/cli.js").then(e => e.default(req));
 });
 
-routes.register("environment", async function(req) {
-	return import("./admin/deployment.js").then(e => e.transferEnvironment(req));
+routes.register("sqlite", async function(req) {
+	return import("./sqlite/cli.js").then(e => e.default(req));
 });
 
-routes.register("export-grammar", thuddar.update);
+routes.register("test", async function(req) {
+	return import("./test/cli.js").then(e => e.default(req));
+});
 
-routes.register("export-sqlite-test", sqlite.main);
+routes.register("thuddar", async function(req) {
+	return import("./thuddar/cli.js").then(e => e.update());
+});
 
-routes.register("works", working.main);
+routes.register("wordbreak/:word?", async function(req) {
+	return import("./wordbreak/cli.js").then(e => e.default(req));
+});
 
-routes.register("wordbreak", async function(req) {
-	return import("./wordbreak/test.js").then(e => e.default(req));
+routes.register("orth/:task?/:name?", async function(req) {
+	return import("./orth/cli.js").then(e => e.default(req));
 });
 
 routes.register("cast/:task?/:name?", async function(req) {
-	return import("./cast/index.js").then(e => e.default(req));
+	return import("./cast/cli.js").then(e => e.default(req));
 });
 
 routes.register("gist/:task?/:name?", async function(req) {
-	return import("./gist/index.js").then(e => e.default(req));
+	return import("./gist/cli.js").then(e => e.default(req));
 });
 
 routes.register("flat/:task?/:name?", async function(req) {
-	return import("./flat/index.js").then(e => e.default(req));
+	return import("./flat/cli.js").then(e => e.default(req));
 });
 
 routes.register("saing/:task?", async function(req) {
-	return import("./saing/index.js").then(e => e.default(req));
+	return import("./saing/cli.js").then(e => e.default(req));
 });
 
 routes.register("sea/:task?/:name?", async function(req) {
-	return import("./sea/index.js").then(e => e.default(req));
+	return import("./sea/cli.js").then(e => e.default(req));
 });
