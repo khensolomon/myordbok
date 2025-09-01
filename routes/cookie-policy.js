cd9b53850db6828852bdbf2d999a49796753518e
Khen Solomon Lethil <khensolomon@gmail.com>
2025-09-01 06:18:39 +0200

    remove routes

--- routes/cookie-policy.js
+++
@@ -1,8 +0,0 @@
-import { server } from "lethil";
-
-const app = server();
-const routes = app.routes("/cookie-policy", "terms");
-
-routes.register({ name: "cookie", text: "Cookie Policy" }, function(req, res) {
-	res.render("cookie-policy", { title: "Cookie Policy - MyOrdbok" });
-});
--- routes/office.js
+++
@@ -1,8 +0,0 @@
-import { server } from "lethil";
-
-const app = server();
-const routes = app.routes("/office", "office");
-
-routes.register({ name: "office", text: "Office" }, function(req, res) {
-	res.render("office", { title: "Office", pageClass: "office" });
-});
