77a869437d67885dad088d517665715d73d9616b
Khen Solomon Lethil <khensolomon@gmail.com>
2025-09-01 06:19:13 +0200

    remove assist

--- assist/setup/ecosystem.js
+++
@@ -1,8 +0,0 @@
-import { deploy } from "lethil";
-
-/**
- * Purely for deployment from local/workspace
- * @alias deploy.ecosystem
- */
-// export default const createOrUpdate = deploy.ecosystem.createOrUpdate;
-export default deploy.ecosystem.createOrUpdate;
--- assist/thuddar/cli.js
+++
@@ -1,1 +0,0 @@
-export { update } from "./base.js";
