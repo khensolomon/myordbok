13aa5e9f34625e312dcb030525ddbb97c378fa25
Khen Solomon Lethil <khensolomon@gmail.com>
2025-09-21 12:23:45 +0200

    add signals

--- core/apps.py
+++ core/apps.py
@@ -1,8 +1,16 @@
 """
-app.py
+App configuration for the 'core' app.
 """
 from django.apps import AppConfig
 
-class MainConfig(AppConfig):
+class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+
+    def ready(self):
+        """
+        This method is called when the Django application is ready.
+        We import our signals here to ensure they are connected.
+        """
+        # Implicitly connect signal handlers decorated with @receiver.
+        from . import signals
